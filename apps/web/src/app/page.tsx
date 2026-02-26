4dab8b2940fdf1d382b6a1ef7b2d225f411a04a7
Rajdeep Das <rajdeepds626@gmail.com>
2026-02-26 15:45:46 +0000

    Switch fonts to DM Sans/DM Mono

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>This page is reserved for landing page.</h1>
+          <h1>This page is reserved for landing page.</h1>
+          <p className="font-mono tracking-tighter">You saved this 63 days ago.</p>
     </div>
   );
 }
