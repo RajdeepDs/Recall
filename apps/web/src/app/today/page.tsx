95e1264bf58e35eead1b60da8a99e63d60850411
Rajdeep Das <rajdeepds626@gmail.com>
2026-02-27 04:59:34 +0000

    add today page

--- apps/web/src/app/today/page.tsx
+++ apps/web/src/app/today/page.tsx
@@ -1,7 +1,29 @@
 export default function TodayPage() {
-    return (
-        <div className="container mx-auto w-full px-6 bg-background">
-            <h1 className="font-medium">Recall</h1>
-        </div>
-    )
+	return (
+		<div className="container mx-auto w-full px-6 pt-16">
+			<header>
+				<h1 className="font-medium">Recall</h1>
+			</header>
+			<main className="mt-24 flex flex-col items-center gap-4">
+				<div className="flex flex-col items-center gap-8">
+					<h2 className="font-mono text-muted-foreground/50 text-xs tracking-tight">
+						You saved this 63 days ago.
+					</h2>
+					<div className="flex flex-col gap-12">
+						<h1 className="text-[1.3rem] leading-[1.4] tracking-tighter sm:text-3xl">
+							What were you trying to fix that night?
+						</h1>
+						<div className="flex flex-col items-center gap-2">
+							<p className="text-sm sm:text-base">
+								Why You’re Always Tired (It’s Not Burnout)
+							</p>
+							<p className="text-muted-foreground text-xs sm:text-sm">
+								A thread about avoidance disguised as productivity.
+							</p>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
