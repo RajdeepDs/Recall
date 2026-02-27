65bed5347929cc97e329a4546497f40a446a19e7
Rajdeep Das <rajdeepds626@gmail.com>
2026-02-27 05:39:41 +0000

    refactor

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,38 +1,38 @@
 import type { Metadata } from "next";
 
-import { DM_Sans, DM_Mono } from "next/font/google";
+import { DM_Mono, DM_Sans } from "next/font/google";
 
 import "../index.css";
 import Providers from "@/components/providers";
 
 const dmSans = DM_Sans({
-  variable: "--font-dm-sans",
-  subsets: ["latin"],
+	variable: "--font-dm-sans",
+	subsets: ["latin"],
 });
 
 const dmMono = DM_Mono({
-    variable: "--font-dm-mono",
-    subsets: ["latin"],
-    weight: "400"
+	variable: "--font-dm-mono",
+	subsets: ["latin"],
+	weight: "400",
 });
 
 export const metadata: Metadata = {
-  title: "Recall",
-  description: "recall",
+	title: "Recall",
+	description: "recall",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${dmSans.variable} ${dmMono.variable} antialiased min-h-svh`} >
-        <Providers>
-            {children}
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={`${dmSans.variable} ${dmMono.variable} min-h-svh antialiased`}
+			>
+				<Providers>{children}</Providers>
+			</body>
+		</html>
+	);
 }
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
-  return (
-    <div>
-          <h1>This page is reserved for landing page.</h1>
-          <p className="font-mono tracking-tighter">You saved this 63 days ago.</p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>This page is reserved for landing page.</h1>
+			<p className="font-mono tracking-tighter">You saved this 63 days ago.</p>
+		</div>
+	);
 }
